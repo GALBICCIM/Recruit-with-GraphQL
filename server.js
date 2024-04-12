cd1a102eaf66e5df3d1541d9004a862c5a2823aa
GALBICCIM <godvictory0909@gmail.com>
2024-04-12 10:26:54 +0900

    Implement Form schema and query

--- server.js
+++ server.js
@@ -4,67 +4,81 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 const typeDefs = gql`
-   type User {
-      id: ID!
-      name: String!
-      student_id: String!
-      phone_number: String!
-      email: String!
-      password: String!
-   }
-   type Query {
-      User: [User]
-   }
-   type Mutation {
-      createUser(name: String!, student_id: String!, phone_number: String!, email: String, password: String!): User
-      modifyUser(id: Int!, student_id: String!, password: String!): User
-      deleteUser(id: Int!): User
-   }
+	type User {
+		id: ID!
+		name: String!
+		student_id: String!
+		phone_number: String!
+		email: String
+		password: String!
+	}
+
+	type Form {
+		id: ID!
+		intro_self: String!
+		because: String!
+		capability: String!
+		ambition: String!
+	}
+
+	type Query {
+		User: [User!s]
+		Form: [Form!]
+	}
+   
+	type Mutation {
+		createUser(name: String!, student_id: String!, phone_number: String!, email: String, password: String!): User
+		modifyUser(id: Int!, student_id: String!, password: String!): User
+		deleteUser(id: Int!): User
+	}
 `;
 
 const resolvers = {
-   Query: {
-      User: (_parent, _args, _context) => {
-         return client.User.findMany();
-      },
-   },
-   Mutation: {
-      createUser: (_parent, { name, student_id, phone_number, email, password }, _context) => {
-         return client.user.create({
-            data: {
-               name,
-               student_id,
-               phone_number,
-               email,
-               password,
-            },
-         });
-      },
-      modifyUser: (_parent, { id, name, student_id, password }, _context) => {
-         return client.User.update({
-            where: {
-               id,
-               student_id,
-               password,
-            },
-            data: {
-               name,
-            },
-         });
-      },
-      deleteUser: (_parent, { id, password }, _context) => {
-         return client.User.delete({
-            where: {
-               id,
-               password,
-            },
-         });
-      },
-   },
+	Query: {
+		User: (_parent, _args, _context) => {
+			return client.User.findMany();
+		},
+		Form: (_parent, _args, _context) => {
+			return client.Form.findMany();
+		}
+	},
+	Mutation: {
+		createUser: (_parent, { name, student_id, phone_number, email, password }, _context) => {
+			return client.user.create({
+				data: {
+					name,
+					student_id,
+					phone_number,
+					email,
+					password
+				}
+			});
+		},
+		modifyUser: (_parent, { id, name, student_id, password }, _context) => {
+			return client.User.update({
+				where: {
+					id,
+					student_id,
+					password
+				},
+				data: {
+					name
+				}
+			});
+		},
+		deleteUser: (_parent, { id, password }, _context) => {
+			return client.User.delete({
+				where: {
+					id,
+					password
+				}
+			});
+		}
+	}
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-   console.log(`Running on ${url}`);
+	console.log(`Running on ${url}`);
 });
